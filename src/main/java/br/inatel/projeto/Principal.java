a7b212453cabbba35f1afbd0ce30bd33b4096c35
DaianeDionisio <90014515+DaianeDionisio@users.noreply.github.com>
2021-11-23 22:07:27 -0300

    teste

--- src/main/java/br/inatel/projeto/Principal.java
+++ src/main/java/br/inatel/projeto/Principal.java
@@ -1,11 +1,13 @@
 package br.inatel.projeto;
 
-import br.inatel.projeto.database.Database;
+import br.inatel.projeto.database.VendedorDB;
 
 public class Principal {
 
     public static void main(String[] args) {
-        Database db = new Database();
-        db.connect();
+
+        VendedorDB vendedorDB = new VendedorDB();
+        Vendedor vendedor1 = new Vendedor("123","Daiane", "35999533564", "MIDC", 2000, "123", 5,500);
+        vendedorDB.insertVendedor(vendedor1);
     }
 }
--- src/main/java/br/inatel/projeto/Vendedor.java
+++ src/main/java/br/inatel/projeto/Vendedor.java
@@ -2,9 +2,20 @@ package br.inatel.projeto;
 
 public class Vendedor extends Funcionario{
 
-    private int numVendas;
+    private int numVendas = 0;
     private int metaMes;
     private float comissao;
+
+    public Vendedor (String cpf, String nome, String telefone, String setor, float salario, String gestor_cpf, int metaMes, float comissao) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.setor = setor;
+        this.salario = salario;
+        this.gestor_cpf = gestor_cpf;
+        this.metaMes = metaMes;
+        this.comissao = comissao;
+    }
 
     public int getNumVendas() {
         return numVendas;
